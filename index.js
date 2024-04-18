15b6ee72d2c59282ee0db1bc3c4a847b7a39dbbe
dhaou290 <dhaouelamri@gmail.com>
2024-04-18 19:59:46 +0100

    implement authentication middleware and controller

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dbConfig = require("./src/config/database.config");
 const dotenv = require("dotenv");
+const userRoute = require("./src/routes/authRoutes");
+const { register, login } = require("./src/controllers/authController");
 const mongoose = require("mongoose");
 
 const app = express();
 dotenv.config();
 app.use(bodyParser.json());
+app.use("/auth", userRoute);
+
+app.use("/auth/register", register);
+app.use("/auth/login", login);
 
 mongoose
   .connect(dbConfig.url, {})
